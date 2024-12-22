27eb4b59dfbd6ec7daeb4cdde38b7393f67b56a0
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2024-12-23 02:48:31 +0600

    volunteer request api create & add to db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -25,17 +25,44 @@ async function run() {
       .db("Volunteer-Management")
       .collection("Volunteer-Need-Post");
 
+    const volunteerRequestCollection = client
+      .db("Volunteer-Management")
+      .collection("Volunteer-Request");
+
     // Get All Volunteer Post
     app.get("/all-volunteer-need-post", async (req, res) => {
       const result = await volunteerNeedPostCollection.find().toArray();
       res.send(result);
     });
-
+    // Get a Specific Post
+    app.get("/volunteer-post/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await volunteerNeedPostCollection.findOne(query);
+      res.send(result);
+    });
     // Volunteer Need Post
     app.post("/volunteer-need-post", async (req, res) => {
       const volunteerNeedPost = req.body;
       const result = await volunteerNeedPostCollection.insertOne(
         volunteerNeedPost
+      );
+      res.send(result);
+    });
+
+    // Be a Volunter Request
+    app.post("/volunteer-request", async (req, res) => {
+      const volunteerReq = req.body;
+      const result = await volunteerRequestCollection.insertOne(volunteerReq);
+      
+      //1. Decrease the volunteer Need count
+      const filter = { _id: new ObjectId(volunteerReq.postId) };
+      const update = {
+        $inc: { numberOfVolunteer: -1 },
+      };
+      const updateCount = await volunteerNeedPostCollection.updateOne(
+        filter,
+        update
       );
       res.send(result);
     });
