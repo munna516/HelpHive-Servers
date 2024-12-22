f0ee464e98caf831dccc9cb49d4f7386ff476a7e
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2024-12-22 17:07:25 +0600

    database setup and add volunteer need post done

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hqlh5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Data Base
+    const volunteerNeedPostCollection = client
+      .db("Volunteer-Management")
+      .collection("Volunteer-Need-Post");
+
+    // Volunteer Need Post
+    app.post("/volunteer-need-post", async (req, res) => {
+      const volunteerNeedPost = req.body;
+      const result = await volunteerNeedPostCollection.insertOne(
+        volunteerNeedPost
+      );
+      res.send(result);
+    });
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is Running");
